904cf05ee3f7c263d9cd681f6211daa3891ea522
carami <carami@nate.com>
2018-06-18 09:54:36 +0900

    day05_01

--- src/interfaceExam/InterfaceB.java
+++ src/interfaceExam/InterfaceB.java
@@ -4,4 +4,8 @@ public interface InterfaceB {
 	default void test() {
 		System.out.println("tettt");
 	}
+	
+	static void m1() {
+		System.out.println("static");
+	}
 }
--- src/interfaceExam/InterfaceTest.java
+++ src/interfaceExam/InterfaceTest.java
@@ -1,12 +1,27 @@
 package interfaceExam;
 
+import java.io.InputStreamReader;
+
 public class InterfaceTest implements InterfaceA, InterfaceB {
 
 	@Override
 	public void test() {
 		// TODO Auto-generated method stub
-		InterfaceA.super.test();
+		InterfaceB.super.test();
 	}
+	
+	public static void main(String[] args) {
+		InterfaceTest t = new InterfaceTest();
+		t.test();
+		//t.m1();
+		InterfaceB.m1();
+		InputStreamReader ir;
+		int[] iarr = new int[4];
+		iarr[4] = 10;
+		System.out.println(iarr[4]);
+	}
+	
+
 
 
 }
